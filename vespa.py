5e7cd7c68fb96a6cfd35ab7625da748da4b389f9
HaRsHGoR <141709893+HaRsHGoRR@users.noreply.github.com>
2023-09-22 18:18:21 +0530

    Add files via upload

---
+++ vespa.py
@@ -0,0 +1,31 @@
+import requests
+import random
+import string
+from concurrent.futures import ProcessPoolExecutor
+
+def generate_random_code(length):
+    code = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
+    return code
+
+def send_request(code):
+    url = "https://wea.vespa.games/api/hongbao/hongbao_detail"
+    headers = {
+        'Token': 'dc30bc30-c42f-42dd-87b2-d0700dedec41',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    }
+
+    payload = {
+        'code': code
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+    print(f"Code: {code}\n{response.text}\n")
+
+code_length = 8
+num_requests = 5  # Number of concurrent requests to send
+
+if __name__ == '__main__':
+    with ProcessPoolExecutor() as executor:
+        while True:
+            random_codes = [generate_random_code(code_length) for _ in range(num_requests)]
+            executor.map(send_request, random_codes)
